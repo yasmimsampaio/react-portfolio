aa45fcbd6ec1227dd1c99d9b435c2a33b1ce6289
Yasmim Sampaio <yasmimsampaio10@hotmail.com>
2021-01-20 22:00:00 -0500

    changed some styling

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./Footer.css"
+import "./Style.css"
 
 class Footer extends Component {
     render(){
--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Style.css"
 
 
 class Home extends Component {
